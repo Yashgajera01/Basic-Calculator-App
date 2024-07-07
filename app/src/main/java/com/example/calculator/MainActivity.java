257987f08001f1110a527bc9fef294dc7e871558
Yashgajera01 <yashgajera2004@gmail.com>
2024-07-07 20:00:02 +0530

    initial commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initialization
+        //initialization.
         decimalFormat = new DecimalFormat("#.##########");
         inputDisplay = findViewById(R.id.input);
         outputDisplay = findViewById(R.id.output);
